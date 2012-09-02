1087a132a8d04189b625dd4f8ee5c53c4cd450e1
moririring <moriri.ring10@gmail.com>
2012-09-02 23:48:00 +0900

    UpdateTest

--- FullAutomationSupportSystemTest/Program.cs
+++ FullAutomationSupportSystemTest/Program.cs
@@ -17,7 +17,6 @@ namespace FullAutomationSupportSystemTest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-
             NUnit.Gui.AppEntry.Main(new string[] { System.Windows.Forms.Application.ExecutablePath, "/run" });
         }
     }
