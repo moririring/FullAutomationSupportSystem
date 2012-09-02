812c1c5d61534804e8ca1d885e1ee07cec23626e
moririring <moriri.ring10@gmail.com>
2012-09-02 23:25:57 +0900

    テストをさらに使いやすく

--- FullAutomationSupportSystemTest/Program.cs
+++ FullAutomationSupportSystemTest/Program.cs
@@ -17,6 +17,8 @@ namespace FullAutomationSupportSystemTest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
+
+            NUnit.Gui.AppEntry.Main(new string[] { System.Windows.Forms.Application.ExecutablePath, "/run" });
         }
     }
 }
