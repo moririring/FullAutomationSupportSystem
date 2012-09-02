e56e82a7d1f969e3ba646e551e2da4f97b72007b
moririring <moriri.ring10@gmail.com>
2012-09-02 23:02:34 +0900

    テスト追加

---
+++ FullAutomationSupportSystem/Task.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FullAutomationSupportSystemTest
+{
+    public class TaskData
+    {
+        public string Name { get; set; }
+        public string OutputFolder { get; set; }
+        public string ProjectFolder { get; set; }
+        public string LogFolder { get; set; }
+        public string Repository { get; set; }
+    }
+
+    public class Task
+    {
+        private List<TaskData> TaskDataList = new List<TaskData>();
+
+        public bool Add(TaskData data)
+        {
+            if (Directory.Exists(data.ProjectFolder) == false)
+            {
+                return false;
+            }
+            TaskDataList.Add(data);
+            return true;
+        }
+
+        public void Clear()
+        {
+            TaskDataList.Clear();
+        }
+
+        public int Count()
+        {
+            return TaskDataList.Count;
+        }
+
+        public TaskData this[int i]
+        {
+            get { return TaskDataList[i]; }
+        }
+    }
+
+}
---
+++ FullAutomationSupportSystemTest/TaskTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FullAutomationSupportSystemTest
+{
+    public class TaskTest
+    {
+        [Test]
+        public void タスクに存在するパスで追加()
+        {
+            Task task = new Task();
+            TaskData data = new TaskData();
+            data.ProjectFolder = @"M:\program\vsht\vsht\VSHackathon20120428\T3";
+            Assert.True(task.Add(data) == true);
+        }
+        [Test]
+        public void タスクに存在しないパスで追加()
+        {
+            Task task = new Task();
+            TaskData data = new TaskData();
+            data.ProjectFolder = @"M:\program\vsht\vsht\VSHackathon20120428\T33";
+            Assert.False(task.Add(data) == true);
+        }
+        [Test]
+        public void タスクをクリア()
+        {
+            Task task = new Task();
+            task.Clear();
+            Assert.True(task.Count() == 0);
+        }
+        [Test]
+        public void タスクの情報取得()
+        {
+            Task task = new Task();
+            TaskData data = new TaskData();
+            data.ProjectFolder = @"M:\program\vsht\vsht\VSHackathon20120428\T3";
+            task.Add(data);
+            Assert.True(task[0].ProjectFolder == data.ProjectFolder);
+        }
+
+    }
+}
