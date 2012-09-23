6cef29456d6bf870da398acf3f430a0b4b5d0482
moririring <moriri.ring10@gmail.com>
2012-09-23 09:07:31 +0900

    Add

---
+++ FullAutomationSupportSystemTest/IniDataTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FullAutomationSupportSystem
+{
+    class IniDataTest
+    {
+        [Test]
+        public void 初期化データ()
+        {
+            IniData ini = new IniData();
+            Assert.True(ini != null);
+        }
+    }
+}
